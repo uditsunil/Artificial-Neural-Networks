31a3ee971bdcdc51fa1f6b9f957e9f4130fd5871
Udit Sunil <udsunil@gmail.com>
2018-07-04 11:59:43 +0530

    Update a_networks.py

--- a_networks.py
+++ a_networks.py
@@ -1,13 +1,3 @@
-   # Artificial Neural Network
-
-# Installing Theano
-# pip install --upgrade --no-deps git+git://github.com/Theano/Theano.git
-
-# Installing Tensorflow
-# pip install tensorflow
-
-# Installing Keras
-# pip install --upgrade keras
 
 # Part 1 - Data Preprocessing
 
